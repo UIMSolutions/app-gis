6402bd6d2274070f6af9d080b21615d8d785f680
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:33:11 +0200

    Update

--- source/apps/gis/package.d
+++ source/apps/gis/package.d
@@ -2,13 +2,33 @@ module apps.gis;
 
 mixin(ImportPhobos!());
 
-public { // required uim libraries
-  import uim.core;
-  import uim.oop;
-  import uim.apps;
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.gis.controllers;
+  import apps.gis.helpers;
+  import apps.gis.tests;
+  import apps.gis.views;
 }
 
-public { // admin-myname libraries
-  import apps.gis.controllers;
-  import apps.gis.views;
+DApp gisApp;
+static this() {
+  gisApp = App
+    .name("gisApp")
+    .rootPath("/apps/gis")
+    .addRoute(Route("", HTTPMethod.GET, GISIndexPageController));
+    .addRoute(Route("/", HTTPMethod.GET, GISIndexPageController));
 }
